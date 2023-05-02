2d6b536c4c8a09b82f449535dc3a8700d4c2ccf6
koba-masa <kobayashi_mshr@koba-masa.com>
2023-05-02 23:14:17 +0900

    feat: GitHubContributionを表示

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GitHubContribution from "@/components/top/GitHubContribution.vue";
+</script>
 
 <template>
   <h1>www.koba-masa.com</h1>
+  <GitHubContribution />
 </template>
 
 <style></style>
