9dbc3bab89aad8fc10b595a2a23b50d93d9a3169
koba-masa <kobayashi_mshr@koba-masa.com>
2024-11-18 03:25:04 +0900

    feat: Starlightをインストールする
    pnpm astro add starlight
    
    https://starlight.astro.build/ja/manual-setup/

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
+import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), starlight()],
 });
