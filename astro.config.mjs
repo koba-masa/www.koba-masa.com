e8125ad41c0f6d3df1ed5fc3a5e12996df54b822
koba-masa <kobayashi_mshr@koba-masa.com>
2024-11-17 21:15:12 +0900

    fix: pnpm format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
+import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
