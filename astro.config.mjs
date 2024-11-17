89e8c50df5f89307772a5e2a29dd8851c337f301
koba-masa <kobayashi_mshr@koba-masa.com>
2024-11-17 21:14:23 +0900

    feat: MDXを追加する

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
 });
